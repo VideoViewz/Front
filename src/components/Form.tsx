7c6b3085d97280a84ed8483d6ee9b56955b67303
Igor <igrr69@gmail.com>
2020-04-01 11:47:54 +0300

    Add upload of file with form-data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,83 +4,91 @@ import { Store } from '../store-folder/Store';
 import { observer } from 'mobx-react';
 
 interface IProps {
-    store: Store;
+  store: Store;
 }
 
 interface IState {
-    file?: File;
-    name?: string;
-    user: string;
-    course: string;
+  file: Blob;
+  name: string;
+  user: string;
+  course: string;
 }
 
 class Form extends React.Component<IProps, IState> {
+  state: IState = {
+    file: new File([''], 'filename'),
+    name: '',
+    user: 'Tomer',
+    course: 'Math'
+  };
 
+  onChangeHandler = (event: any) => {
+    console.log(event.target.files[0]);
+    this.setState({
+      file: event.target.files[0],
+      name: event.target.files[0].name
+    });
+  };
 
-    state: IState = {
-        file: undefined,
-        name: '',
-        user: 'Tomer',
-        course: 'Math'
-    }
+  handleSubmit = () => {
+    console.log(this.state.file);
+    const formData = new FormData();
+    formData.append('file', this.state.file);
+    formData.append('name', this.state.name);
+    formData.append('user', this.state.user);
+    formData.append('course', this.state.course);
 
-    onChangeHandler = (event: any) => {
-        this.setState({ file: event.target.files[0], name: event.target.files[0].name });
-    }
+    axios.post(`http://localhost:3000/video/upload`, formData).then(res => {
+      console.log(res);
+      console.log(res.data);
+    });
+  };
 
-    handleSubmit = () => {
-        axios.post(`https://jsonplaceholder.typicode.com/users`, {
-            file: this.state.file,
-            name: this.state.name,
-            user: this.state.user,
-            course: this.state.course
-        })
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="row paddingNav">
-                    <div className="col-lg-3">
-                        <img className="imgStyle" src={require(`../pictures/logoVideo.png`)} alt="" />
-                    </div>
-                    <div className="col-lg-3">
-                        nav template
-                    </div>
-                    <div className="col-lg-3">
-                        nav template
-                    </div>
-                    <div className="col-lg-3">
-                        nav template
-                    </div>
-                </div>
-                <div className="row backgroundImgStyle">
-                    <div className="col-12">
-                        <div className="row">
-                            <div className="col-lg-12 ">
-                                <h1 className="uploadVideo">Upload Video</h1>
-                            </div>
-                        </div>
-                        <div style={{ padding: '13px' }} className="row">
-                            <div className="col-lg-12">
-                                <button onClick={() => this.handleSubmit()} className="uploadButtonStyle">
-                                    <h1 className="plusStyle">+</h1>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <input type="file" name="file" onChange={this.onChangeHandler} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="row paddingNav">
+          <div className="col-lg-3">
+            <img
+              className="imgStyle"
+              src={require(`../pictures/logoVideo.png`)}
+              alt=""
+            />
+          </div>
+          <div className="col-lg-3">nav template</div>
+          <div className="col-lg-3">nav template</div>
+          <div className="col-lg-3">nav template</div>
+        </div>
+        <div className="row backgroundImgStyle">
+          <div className="col-12">
+            <div className="row">
+              <div className="col-lg-12 ">
+                <h1 className="uploadVideo">Upload Video</h1>
+              </div>
             </div>
-        )
-    }
+            <div style={{ padding: '13px' }} className="row">
+              <div className="col-lg-12">
+                <button
+                  onClick={() => this.handleSubmit()}
+                  className="uploadButtonStyle"
+                >
+                  <h1 className="plusStyle">+</h1>
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-12">
+                <input
+                  type="file"
+                  name="file"
+                  onChange={this.onChangeHandler}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default observer(Form);
