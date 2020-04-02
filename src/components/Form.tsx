861be555a4056991aebf14a5c0f7a28119d8e4f7
Tomer <tomerst3@gmail.com>
2020-04-02 13:14:14 +0300

    Continue UI of teacher

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import ReactPlayer from 'react-player';
 import { Store } from '../store-folder/Store';
 import { observer } from 'mobx-react';
 
@@ -48,20 +47,37 @@ class Form extends React.Component<IProps, IState> {
   render() {
     return (
       <div>
-        <div className="row paddingNav">
-          <div className="col-lg-3">
-            <img
-              className="imgStyle"
-              src={require(`../pictures/logoVideo.png`)}
-              alt=""
-            />
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <img
+            className="imgStyle"
+            src={require(`../pictures/logoVideo.png`)}
+            alt=""
+          />
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
           </div>
-          <div className="col-lg-3">nav template</div>
-          <div className="col-lg-3">nav template</div>
-          <div className="col-lg-3">nav template</div>
-        </div>
+        </nav>
         <div className="row backgroundImgStyle">
-          <div className="col-12">
+          <div className="col-lg-12">
             <div className="row">
               <div className="col-lg-12 ">
                 <h1 className="uploadVideo">Upload Video</h1>
@@ -87,11 +103,14 @@ class Form extends React.Component<IProps, IState> {
               </div>
             </div>
             <div className="row">
-              <div className="col-lg-12">
-                <ReactPlayer url='https://www.youtube.com/watch?v=OboAhfz-kko' playing />
+              <div className="col-md-3"></div>
+              <div style={{marginBottom:"40px"}} className="col-md-6">
+                <div className="embed-responsive embed-responsive-16by9">
+                  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/zlRq8RAawng" allowFullScreen></iframe>
+                </div>
               </div>
+              <div className="col-md-3"></div>
             </div>
-
           </div>
         </div>
       </div>
