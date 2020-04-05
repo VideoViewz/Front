dcaea08a398e8aa4e489f0ff7025dde1e78d64f1
Tomer <tomerst3@gmail.com>
2020-04-05 15:27:33 +0300

    Updates

---
+++ precache-manifest.b2b9ff3dffc9e302d25cae05b8359706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572c1d88dbf0beca0e1e72adc4c6f750",
+    "url": "/VideoView-GitHub/index.html"
+  },
+  {
+    "revision": "4829651d18cd69147476",
+    "url": "/VideoView-GitHub/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b50eb268fcf2d823d2e4",
+    "url": "/VideoView-GitHub/static/css/main.7728337f.chunk.css"
+  },
+  {
+    "revision": "4829651d18cd69147476",
+    "url": "/VideoView-GitHub/static/js/2.7706a379.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "/VideoView-GitHub/static/js/2.7706a379.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b50eb268fcf2d823d2e4",
+    "url": "/VideoView-GitHub/static/js/main.444c5c78.chunk.js"
+  },
+  {
+    "revision": "f803d03091040bd1252e",
+    "url": "/VideoView-GitHub/static/js/runtime-main.36219a59.js"
+  }
+]);
